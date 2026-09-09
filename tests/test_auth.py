d1f28bec90d1e6d5a492919ef0a9370f4e328219
JuanASanta <sleonjuanantonio@gmail.com>
2026-09-09 18:43:23 +0200

    Testing login with Pytest

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -15,4 +15,17 @@ def test_register_user(api_client):
 
     assert response.status_code == 201
     assert get_user_model().objects.filter(username="testuser").exists()
-    
+
+
+@pytest.mark.django_db
+def test_login_user(api_client, create_user):
+    username = "testuser"
+    password = "12345678"
+
+    create_user(username=username, password=password)
+
+    data = {"username": username, "password": password}
+    response = api_client.post("/api/login/", data, format="json")
+
+    assert response.status_code == 200
+    assert "token" in response.data
